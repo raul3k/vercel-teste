a01f3fac2ac46b4620869c046e5faf89d4681303
Raul Souza <raul.3k@gmail.com>
2020-11-30 15:43:15 -0300

    Delay

--- pages/tempo.js
+++ pages/tempo.js
@@ -5,12 +5,18 @@ function Tempo(props){
     return (
         <div>
             <div>{dynamicDateString} (dinamico)</div>
-            <div>{props.staticDateString} (estatico)</div>
+            <div>{props.staticDateString} (estatico - com delay)</div>
         </div>
     )
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+    console.log("> Adicionando delay de 5 segundos.");
+
+    await delay(5000);
+
+    console.log("> Depois delay de 5 segundos.");
+
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
 
@@ -20,5 +26,7 @@ export function getStaticProps() {
         }
     };
 }
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default Tempo;
